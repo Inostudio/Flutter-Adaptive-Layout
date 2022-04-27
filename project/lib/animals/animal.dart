196903ef6fa2780a71b45ef913e4a099d9922fbc
Maxim Magin <m.magin@inostudio.com>
2022-04-27 15:36:53 +0300

    add drafts

---
+++ project/lib/animals/animal.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class Animal {
+  String breed;
+  Image image;
+  String shortDescription;
+  String description;
+
+  Animal({
+    required this.breed,
+    required this.image,
+    required this.shortDescription,
+    required this.description,
+  });
+}
---
+++ project/lib/constants/strings.dart
@@ -0,0 +1,10 @@
+class Strings {
+  static const assetPath = "assets/";
+  static catImage(int index) => assetPath + "cats/cat$index.png";
+  static dogImage(int index) => assetPath + "dogs/dog$index.png";
+  static parrotImage(int index) => assetPath + "parrots/parrot$index.png";
+  static inoLogo() => assetPath + "ino.png";
+  static String cats = "cats";
+  static String dogs = "dogs";
+  static String parrots = "parrots";
+}
